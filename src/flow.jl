1a317c33441fd4663fccda66e41d408d34f59e5f
AtilaSaraiva <atilasaraiva@gmail.com>
2024-09-26 04:51:14 -0600

    docs: adding docstring for @flow macro

--- src/flow.jl
+++ src/flow.jl
@@ -44,6 +44,29 @@ function flow(path::Expr, expr::Expr)
     end
 end
 
+"""
+    @flow path="path/to/file" expr
+
+A macro that checks if the file in the `path` exists and if it didn't change and only runs the `expr` if needed. After the first run, create a file with the same name as `path` with the .hash suffix, and writes the sha256 sum hash.
+
+# Example
+
+```jldoctest
+julia> filePath, _ = mktemp();
+
+julia> @flow path=filePath begin
+           println("This should only show once")
+           write(filePath, "some random text")
+       end
+This should only show once
+64
+
+julia> @flow path=filePath begin
+           println("This should only show once")
+           write(filePath, "some random text")
+       end
+```
+"""
 macro flow(path::Expr, expr::Expr)
     if path.args[1] != :path
         error("Missing path keyword argument")
