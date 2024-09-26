3ee70e3e1b39f937fdfeba343066f51ef0e6f81d
AtilaSaraiva <atilasaraiva@gmail.com>
2024-09-26 03:49:28 -0600

    changing macro to just output expression while avoiding doing computation during the macro execution

--- src/flow.jl
+++ src/flow.jl
@@ -14,45 +14,72 @@ function checkFileExists(path, sha256sum)
     end
 end
 
-function runIfChanged(path::String, codeBlock::Expr)
-    # Define the hash file path
-    hash_file = path * ".hash"
+# function runIfChanged(path::String, codeBlock::Expr)
+    # # Define the hash file path
+    # hash_file = path * ".hash"
 
-    # Check if hash file exists
-    if isfile(hash_file)
-        # Read the saved hash
-        saved_hash = read(hash_file, String)
+    # # Check if hash file exists
+    # if isfile(hash_file)
+        # # Read the saved hash
+        # saved_hash = read(hash_file, String)
 
-        # Check if the file exists and hash matches
-        if checkFileExists(path, saved_hash)
-            return
-        end
+        # # Check if the file exists and hash matches
+        # if checkFileExists(path, saved_hash)
+            # return
+        # end
+    # end
+
+    # runPost = quote
+        # # Compute new hash and save it
+        # new_hash = open(path) do io
+            # io |> sha256 |> bytes2hex
+        # end
+        # open($hash_file, "w") do io
+            # write(io, new_hash)
+        # end
+    # end
+
+    # fullExpr = Expr(:block, esc(codeBlock), runPost)
+
+    # fullExprWithPath = postwalk(x -> x == :path ? path : x, fullExpr)
+
+    # return fullExprWithPath
+# end
+
+function flow(path::Expr, expr::Expr)
+    return quote
+       filePath = $(path.args[2])
+       hash_file = filePath * ".hash"
+       # Check if hash file exists
+       flag = true
+       if isfile(hash_file)
+           # Read the saved hash
+           saved_hash = read(hash_file, String)
+
+           # Check if the file exists and hash matches
+           if $checkFileExists(filePath, saved_hash)
+               flag = false
+           end
+       end
+
+       if flag
+           $(esc(expr))
+
+           # Compute new hash and save it
+           new_hash = open(filePath) do io
+               io |> $sha256 |> $bytes2hex
+           end
+           open(hash_file, "w") do io
+               write(io, new_hash)
+           end
+       end
     end
-
-    runPost = quote
-        # Compute new hash and save it
-        new_hash = open(path) do io
-            io |> sha256 |> bytes2hex
-        end
-        open($hash_file, "w") do io
-            write(io, new_hash)
-        end
-    end
-
-    fullExpr = Expr(:block, esc(codeBlock), runPost)
-
-    fullExprWithPath = postwalk(x -> x == :path ? path : x, fullExpr)
-
-    return fullExprWithPath
 end
 
-macro flow(expr1::Expr, expr2::Expr)
-    if expr1.args[1] != :path
+macro flow(path::Expr, expr::Expr)
+    if path.args[1] != :path
         error("Missing path keyword argument")
-    elseif !(expr1.args[2] isa String)
-        error("The path value is not a String")
     end
 
-    path = expr1.args[2]
-    return runIfChanged(path, expr2)
+    return flow(path, expr)
 end
