3a5042341ccc5191c845342e094ebee689c03625
AtilaSaraiva <atilasaraiva@gmail.com>
2024-09-26 03:57:52 -0600

    tests: writing tests for the @flow macro

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,20 @@ using Flow
 using Test
 
 @testset "Flow.jl" begin
-    # Write your tests here.
+    counter = 1
+
+    filePath, _ = mktemp()
+    @flow path=filePath begin
+        counter += 1
+        write(path, "oi1")
+    end
+
+    @flow path=filePath begin
+        counter += 1
+    end
+
+    rm(filePath)
+    rm(filePath * ".hash")
+
+    @test counter == 2
 end
